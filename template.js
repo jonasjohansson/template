a89645647877f02e0801771c047f758aced27a9f
Jonas Johansson <jns.johansson@gmail.com>
2019-09-11 22:19:53 +0200

    Update template.js

--- template.js
+++ template.js
@@ -8,7 +8,7 @@ function init() {
 			fig.classList.remove('loading');
 		};
 		let title = getTitle(image.src).trim();
-		if (image.alt.length !== 0) return;
+		if (image.alt.length !== 0) continue;
 		image.alt = title;
 		image.title = title;
 		let figCaption = document.createElement('figcaption');
