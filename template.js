d4bf5699325f0d701b8de4b696d8f99878f8b06d
Jonas Johansson <jns.johansson@gmail.com>
2023-10-23 19:20:12 +0200

    Update template.js

--- template.js
+++ template.js
@@ -1,29 +1,31 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init)
 
 function init() {
-	for (let fig of document.querySelectorAll('figure')) {
-		fig.classList.add('loading');
-		let image = fig.querySelector('img');
-		image.onload = function() {
-			fig.classList.remove('loading');
-		};
-		let title = getTitle(image.src).trim();
-		if (image.alt.length !== 0) continue;
-		image.alt = title;
-		image.title = title;
-		let figCaption = document.createElement('figcaption');
-		figCaption.innerHTML = title;
-		fig.appendChild(figCaption);
+	const figs = document.querySelectorAll('figure')
+	for (const fig of figs) {
+		fig.classList.add('loading')
+		let image = fig.querySelector('img')
+		image.onload = function () {
+			image.parentElement.classList.remove('loading')
+		}
+		let title = getTitle(image.src).trim()
+		if (image.alt.length !== 0) continue
+		image.alt = title
+		image.title = title
+		let figCaption = document.createElement('figcaption')
+		figCaption.innerHTML = title
+		fig.appendChild(figCaption)
 	}
 	for (let a of document.querySelectorAll('p a')) {
-		a.target = '_blank';
+		a.target = '_blank'
 	}
 }
 
 function getTitle(string) {
-	let ext = string.split('.').pop();
-	let path = string.substring(string.indexOf('-') + 1);
-	let title = path.slice(0, -ext.length - 1);
-	title = title.replace(/_/g, ' ');
-	return decodeURI(title);
+	let ext = string.split('.').pop()
+	let path = string.substring(string.indexOf('-') + 1)
+	let title = path.slice(0, -ext.length - 1)
+	title = title.replace(/_/g, ' ')
+	return decodeURI(title)
 }
+
