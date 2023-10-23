3d7ecfeb4c88ccfdc519845fbd7767a04643d92e
Jonas Johansson <jns.johansson@gmail.com>
2023-10-23 21:17:41 +0200

    Update template.js

--- template.js
+++ template.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', init)
 function init() {
 	const figs = document.querySelectorAll('figure')
 	for (const fig of figs) {
-		fig.classList.add('loading')
+		// fig.classList.add('loading')
 		let image = fig.querySelector('img')
-		image.onload = function (fig) {
-			fig.classList.remove('loading')
-		}
+		// image.onload = function (fig) {
+		// 	fig.classList.remove('loading')
+		// }
 		let title = getTitle(image.src).trim()
 		if (image.alt.length !== 0) continue
 		image.alt = title
